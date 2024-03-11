f05eccf6f29beb4f2a24ff89c0064add0310b3cb
Eduardo Neves Paschoal <enp396@gmail.com>
2024-03-11 15:00:20 -0300

    New base website

--- src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Text } from "./Text";
-import { Heading } from "./Heading";
-import { Img } from "./Img";
-import { Button } from "./Button";
-import { ReactTable } from "./ReactTable";
-export { Text, Heading, Img, Button, ReactTable };
